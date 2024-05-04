7446d35558008c339fb0e07783901d4112720f81
YiggidyYi <105264876+YiggidyYi@users.noreply.github.com>
2024-05-03 20:25:03 -0700

    bugfixes!

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -12,7 +12,7 @@ export default function Meals({
     <div className={"meal-item"}>
       <img src={`http://localhost:3000/${image}`}></img>
       <h3>{name}</h3>
-      <p className="meal-item-price">{price}</p>
+      <p className="meal-item-price">${price}</p>
       <p className="meal-item-description">{description}</p>
       <button className="button meal-item-actions" onClick={() => addMeal(id)}>
         Add to Cart
